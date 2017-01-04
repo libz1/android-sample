6c152bf5684f5787ceedbdcc2bfde75bfffa0b34
xuky <x_ky@sina.com>
2017-01-04 16:46:03 +0800

    Initial commit

---
+++ app/src/main/java/helloworld/xuky/com/xuky03/XukyNote.java
@@ -0,0 +1,25 @@
+package helloworld.xuky.com.xuky03;
+
+/**
+ * Created by Administrator on 2017/1/4.
+ */
+public class XukyNote {
+    int id;
+    String msg;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
